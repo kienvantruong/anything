16f535310883042815da266533e6abe289f7fd71
kienvantruong <van.vanveo.2022@gmail.com>
2025-08-27 12:53:15 +0700

    cô tuyến và jun 27/8 	tập tin mới:   python/OCR-jun.py 	tập tin mới:   python/cung_nhan_ma.py 	đã sửa:        python/test.py 	tập tin mới:   your_url.png

--- python/test.py
+++ python/test.py
@@ -1,7 +1,14 @@
-import time
-
-for i in range(10):
-    i = (i+1)% 2
-    time.sleep(1)
-    print(i)
-# % là tính số dư: VD:7 % 3 = 1 (7 chia 3 bằng 2 dư 1)
+def date(ngay,thang):
+    if (thang == 11 and ngay >= 15) or (thang == 12 and ngay <= 15):
+        return print("bạn là cung nhân mã")
+    else:
+        return print("bạn không phải là cung nhân mã")
+try:
+    ngay = int(input("ngày sinh của bạn là:"))
+    thang = int(input("tháng sinh của bạn là:"))    
+    if 1 <= thang <= 12 and 1 <= ngay <= 31:
+        print(date(ngay,thang))
+    else:
+        print("ngày tháng không hộp lệ")
+except ValueError:
+    print("😑 vui làng nhập lại")
